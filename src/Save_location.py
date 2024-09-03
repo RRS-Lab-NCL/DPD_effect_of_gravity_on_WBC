8419e7f2aa7ed30f19392d8a66d4d915c8a297c0
RRS-Lab-NCL <95027088+RRS-Lab-NCL@users.noreply.github.com>
2024-09-03 13:08:00 +0530

    Update Save_location.py

--- src/Save_location.py
+++ src/Save_location.py
@@ -6,8 +6,8 @@ def Data_path(Parameters):
     now                 = datetime.now()
     sim_date            = str(now).replace(" ", "_")
     sim_date            = str(sim_date).replace(":", "-")
-    current_dir 	    = os.path.dirname(os.path.abspath(__file__))
-    base_dir 		    = os.path.dirname(current_dir)
+    current_dir 	 = os.path.dirname(os.path.abspath(__file__))
+    base_dir 		 = os.path.dirname(current_dir)
     path                = base_dir + '/Data_dump/DPD_data_' + Parameters.F_ext + "g_" + Parameters.Body_force + '_pressure_' + sim_date + sim_date
     
     if not os.path.exists(path):
