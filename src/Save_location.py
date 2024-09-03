ecd14c15f694f4d29815c7b1d0141b0560849704
RRS-Lab-NCL <95027088+RRS-Lab-NCL@users.noreply.github.com>
2024-09-03 13:07:46 +0530

    Update Save_location.py

--- src/Save_location.py
+++ src/Save_location.py
@@ -6,7 +6,9 @@ def Data_path(Parameters):
     now                 = datetime.now()
     sim_date            = str(now).replace(" ", "_")
     sim_date            = str(sim_date).replace(":", "-")
-    path                = "/scratch/users/ramrup/Anirudh/DPD_py/FSI_Data_dump_" + Parameters.F_ext + "g_" + Parameters.Body_force + '_pressure_' + sim_date + sim_date
+    current_dir 	    = os.path.dirname(os.path.abspath(__file__))
+    base_dir 		    = os.path.dirname(current_dir)
+    path                = base_dir + '/Data_dump/DPD_data_' + Parameters.F_ext + "g_" + Parameters.Body_force + '_pressure_' + sim_date + sim_date
     
     if not os.path.exists(path):
         mkdir           = os.mkdir(path, mode = 0o777)
