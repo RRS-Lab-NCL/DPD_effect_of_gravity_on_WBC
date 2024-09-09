8de54ab0c2f763d250b4d9eaba7b10fc7b80f60a
RRS-Lab-NCL <95027088+RRS-Lab-NCL@users.noreply.github.com>
2024-09-09 12:09:06 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  9 11:51:21 2024
+
+@author: itd-2495
+"""
+
 """
 Main file: requires two inputs: 
 	body_force (external force applied in x-axis; datatype: float)
@@ -8,13 +16,11 @@ Dependencies:
 	Cython 3.0.11 convert .pyx files to c extension files (pip install Cython); 
 	
 """
+import os
 import sys
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+import pickle
 import numpy as np
 
-from tqdm import tqdm
-from matplotlib.colors import Normalize
 from Parameters import Simulation_Parameters
 from Fluid import Fluid_init
 from Solid_WBC import WBC_init
@@ -24,58 +30,104 @@ from Physics_engine import Equation_2D_fluid, Equation_2D_WBC
 from Data_dump import Data_parameters, Data_pickling, Data_WBC
 from Save_location import Data_path
 
+def load_checkpoint(path):
+    
+    try:
+        with open(os.path.join(path, 'last_checkpoint.pkl'), 'rb') as f:
+            checkpoint      = pickle.load(f)
+        print("Resuming from saved data...")
+        return checkpoint
+    
+    except FileNotFoundError:
+        print("No checkpoint found, starting a new simulation...")
+        return None
+
+def save_checkpoint(path, Fluid, Solid_WBC, ts):
+    
+    checkpoint = {'Fluid': Fluid,
+                  'Solid_WBC': Solid_WBC,
+                  'ts': ts
+                  }
+    
+    with open(os.path.join(path, 'last_checkpoint.pkl'), 'wb') as f:
+        pickle.dump(checkpoint, f)
+
 def main(body_force, multiple):
 
     parameters                      = Simulation_Parameters(body_force, multiple)				# Initiates parameter set
-    Fluid                           = Fluid_init(parameters)							# Initiates Fluid particles from Fluid module
-        
-    Solid_WBC                       = WBC_init(parameters)							# Initiates WBC from Solid module
+    path                            = Data_path(parameters)                                     # Path for saving data        
+    checkpoint                      = load_checkpoint(path)                                     # Attempt to load previous checkpoint
     
-    xi_WBC                          = Xi(len(Solid_WBC[0].pos)) 						# Preallocates (n x n) random number set for solid particles
+    if checkpoint:
+        Fluid                       = checkpoint['Fluid']
+        Solid_WBC                   = checkpoint['Solid_WBC']
+        start_ts                    = checkpoint['ts'] + 1                                      # Resume from next step
+    else:
+        Fluid                       = Fluid_init(parameters)
+        Solid_WBC                   = WBC_init(parameters)
+        start_ts                    = 0
     
-    fcalc_params_fluid              = Force_calc_params(parameters, Fluid) 					# Initiates DPD parameters (aij, gammaij, sigmaij) for fluid
-    fcalc_params_WBC                = Force_calc_params(parameters, Solid_WBC)				# Initiates DPD parameters (aij, gammaij, sigmaij) for solid
+    Fluid                           = Fluid_init(parameters)							        # Initiates Fluid particles from Fluid module       
+    Solid_WBC                       = WBC_init(parameters)						            	# Initiates WBC from Solid module
     
-    path                            = Data_path(parameters)							# Initiates path to save the trajectories
-    saved_path                      = Data_parameters(path, parameters, Fluid)				# Saves simulation's metadata
+    xi_WBC                          = Xi(len(Solid_WBC[0].pos)) 				        		# Preallocates (n x n) random number set for solid particles
+    
+    fcalc_params_fluid              = Force_calc_params(parameters, Fluid) 		    			# Initiates DPD parameters (aij, gammaij, sigmaij) for fluid
+    fcalc_params_WBC                = Force_calc_params(parameters, Solid_WBC)	     			# Initiates DPD parameters (aij, gammaij, sigmaij) for solid
+    
+    path                            = Data_path(parameters)					            		# Initiates path to save the trajectories
+    saved_path                      = Data_parameters(path, parameters, Fluid)  				# Saves simulation's metadata
+    
+    try:
 
-    for ts in (range(int(parameters.nt))):									# Simulation start; Time marching
-        
-        ############################## FIRST VERLET STEP ############################################################
-    
-        Fluid                       = Equation_2D_fluid.VV_update1(Fluid, parameters)
-        Solid_WBC                   = Equation_2D_WBC.VV_update1(Solid_WBC, parameters)
-        
-        ############################## COMPUTING DPD FORCES #########################################################
-                                                                    
-        Fluid                       = Equation_2D_fluid.Compute_forces(Fluid, Solid_WBC, {},
-                                                                        parameters,fcalc_params_fluid)
-        Solid_WBC                   = Equation_2D_WBC.Compute_forces(Solid_WBC, Fluid, parameters, 
-                                                                      fcalc_params_WBC, xi_WBC)
-        
-        ############################## COMPUTING WBC INTERNAL FORCES ################################################
-        
-        Solid_WBC                   = Equation_2D_WBC.Internal_Forces(Solid_WBC, parameters)
-        
-        ############################## SECOND VERLET STEP ###########################################################
-        
-        Fluid                       = Equation_2D_fluid.VV_update2(Fluid, parameters, ts)        
-        Solid_WBC                   = Equation_2D_WBC.VV_update2(Solid_WBC, parameters)
-        
-        ############################## SAVING SIMULATION DATA #######################################################
-                
-        if (np.mod(ts, 250) == 0):
+        for ts in (range(start_ts, int(parameters.nt))):								        # Simulation start; Time marching
             
-            saved_path              = Data_pickling(path, Fluid, ts)
+            ############################## FIRST VERLET STEP ############################################################
+        
+            Fluid                       = Equation_2D_fluid.VV_update1(Fluid, parameters)
+            Solid_WBC                   = Equation_2D_WBC.VV_update1(Solid_WBC, parameters)
+            
+            ############################## COMPUTING DPD FORCES #########################################################
+                                                                        
+            Fluid                       = Equation_2D_fluid.Compute_forces(Fluid, Solid_WBC, {},
+                                                                            parameters,fcalc_params_fluid)
+            Solid_WBC                   = Equation_2D_WBC.Compute_forces(Solid_WBC, Fluid, parameters, 
+                                                                          fcalc_params_WBC, xi_WBC)
+            
+            ############################## COMPUTING WBC INTERNAL FORCES ################################################
+            
+            Solid_WBC                   = Equation_2D_WBC.Internal_Forces(Solid_WBC, parameters)
+            
+            ############################## SECOND VERLET STEP ###########################################################
+            
+            Fluid                       = Equation_2D_fluid.VV_update2(Fluid, parameters, ts)        
+            Solid_WBC                   = Equation_2D_WBC.VV_update2(Solid_WBC, parameters)
+            
+            ############################## SAVING SIMULATION DATA #######################################################
+                    
+            if (np.mod(ts, 250) == 0):
                 
-            for nwbc in range(len(Solid_WBC)):
-                obj                 = Solid_WBC[nwbc]
-                saved_path          = Data_WBC(path, obj, nwbc, ts)                
-                
-            print(ts, Fluid.KE[ts], Fluid.kBT[ts])
+                saved_path              = Data_pickling(path, Fluid, ts)
+                    
+                for nwbc in range(len(Solid_WBC)):
+                    obj                 = Solid_WBC[nwbc]
+                    saved_path          = Data_WBC(path, obj, nwbc, ts)                
+                    
+                print(ts, Fluid.KE[ts], Fluid.kBT[ts])               
+                save_checkpoint(path, Fluid, Solid_WBC, ts)                                     # Save checkpoint periodically
+
+    except Exception as e:
+        
+        print(f"An error occurred at time step {ts}: {e}")
+        print("Exiting the current cycle and restarting from the previous checkpoint...")
+        sys.exit(1)                                                                             # Exit the program to allow a safe restart
             
 if __name__ == "__main__":
     
-    body_force  = sys.argv[1]
-    multiple    = sys.argv[2]
-    op          = main(float(body_force), float(multiple))
+    if len(sys.argv) < 3:
+        print("Usage: python simulation.py <body_force> <multiple>")
+        sys.exit(1)
+    
+    body_force      = sys.argv[1]
+    multiple        = sys.argv[2]
+    main(float(body_force), float(multiple))
